4f6f57a437a4167ef270e0c08ae6d968f2dfc246
EricNavar <ericnavar@ufl.edu>
2021-09-05 13:57:36 -0400

    add peerId attribute to Logger class

--- Logger.java
+++ Logger.java
@@ -1,60 +1,78 @@
 import java.util.ArrayList;
+import java.io.FileOutputStream;  
 
 class Logger {
-    public void onConnectingTo (String time, String peer_ID_1, String peer_ID_2) {
-        log(time + ": Peer " + peer_ID_1 + " makes a connection to Peer " + peer_ID_2 + ".");
+    String peerID;
+
+    public Logger(String peerID) {
+        this.peerID = peerID;
     }
 
-    public void onConnectingFrom (String time, String peer_ID_1, String peer_ID_2) {
-        log(time + ": Peer " + peer_ID_1 + " is connected from Peer " + peer_ID_2 + ".");
+    public void onConnectingTo (String time, String peerID2) {
+        log(time + ": Peer " + this.peerID + " makes a connection to Peer " + peerID2 + ".");
     }
 
-    //could be made quicker with string builders, but who cares
-    public void onChangeOfPreferredNeighbors (String time, String peer_ID, ArrayList<String> preferredNeighbors) {
-        String toPrint = time + ": Peer " + peer_ID + " has the preferred neighbors ";
+    public void onConnectingFrom (String time, String peerID2) {
+        log(time + ": Peer " + this.peerID + " is connected from Peer " + peerID2 + ".");
+    }
+
+    public void onChangeOfPreferredNeighbors (String time, ArrayList<String> preferredNeighbors) {
+        StringBuilder toPrint = new StringBuilder(time + ": Peer " + this.peerID + " has the preferred neighbors ");
         for (int i = 0; i < preferredNeighbors.size(); i++) {
             if (i != 0) {
-                toPrint += ", ";
+                toPrint.append(", ");
             }
-            toPrint += preferredNeighbors.get(i);
+            toPrint.append(preferredNeighbors.get(i));
         }
-        toPrint += ".";
-        log(toPrint);
+        toPrint.append(".");
+        log(toPrint.toString());
     }
 
-    public void onChangeOfOptimisticallyUnchokedNeighbor(String time, String peer_ID, String optimisticallyUnchockedNeighbor_ID) {
-        log(time + ": Peer " + peer_ID + "has the optimistically unchoked neighbor " + optimisticallyUnchockedNeighbor_ID + ".");
+    public void onChangeOfOptimisticallyUnchokedNeighbor(String time, String optimisticallyUnchockedNeighbor_ID) {
+        log(time + ": Peer " + this.peerID + "has the optimistically unchoked neighbor " + optimisticallyUnchockedNeighbor_ID + ".");
     } 
 
-    public void onUnchoking(String time, String peer_ID_1, String peer_ID_2) {
-        log(time + ": Peer " + peer_ID_1 + " is unchoked by " + peer_ID_2 + ".");
+    public void onUnchoking(String time, String peerID2) {
+        log(time + ": Peer " + this.peerID + " is unchoked by " + peerID2 + ".");
     }
 
-    public void onChoking(String time, String peer_ID_1, String peer_ID_2) {
-        log(time + ": Peer " + peer_ID_1 + " is choked by " + peer_ID_2 + ".");
+    // choke me plz
+    public void onChoking(String time, String peerID2) {
+        log(time + ": Peer " + this.peerID + " is choked by " + peerID2 + ".");
     }
 
-    public void onReceiveHaveMessage(String time, String peer_ID_1, String peer_ID_2, String pieceIndex) {
-        log(time + ": Peer " + peer_ID_1 + " received the  ‘have’ message from " + peer_ID_2 + " for the piece " + pieceIndex + ".");
+    public void onReceiveHaveMessage(String time, String peerID2, String pieceIndex) {
+        log(time + ": Peer " + this.peerID + " received the  ‘have’ message from " + peerID2 + " for the piece "
+                + pieceIndex + ".");
     }
 
-    public void onReceiveInterestedMessage(String time, String peer_ID_1, String peer_ID_2) {
-        log(time + ": Peer " + peer_ID_1 + " received the ‘interested’ message from " + peer_ID_2 + ".");
+    public void onReceiveInterestedMessage(String time, String peerID2) {
+        log(time + ": Peer " + this.peerID + " received the ‘interested’ message from " + peerID2 + ".");
     }
 
-    public void onReceiveNotInterestedMessage(String time, String peer_ID_1, String peer_ID_2) {
-        log(time + ": Peer " + peer_ID_1 + " received the ‘not interested’ message from " + peer_ID_2 + ".");
+    public void onReceiveNotInterestedMessage(String time, String peerID2) {
+        log(time + ": Peer " + this.peerID + " received the ‘not interested’ message from " + peerID2 + ".");
     }
 
-    public void onDownloadingAPiece(String time, String peer_ID_1, String peer_ID_2, int piece_index, int number_of_pieces) {
-        log(time + ": Peer " + peer_ID_1 + " has downloaded the piece " + String.valueOf(piece_index) + " from " + peer_ID_2 + ". Now the number of pieces it has is " + String.valueOf(number_of_pieces) + ".");
+    public void onDownloadingAPiece(String time, String peerID2, int piece_index, int number_of_pieces) {
+        log(time + ": Peer " + this.peerID + " has downloaded the piece " + String.valueOf(piece_index) +
+                " from " + peerID2 + ". Now the number of pieces it has is " + String.valueOf(number_of_pieces) + ".");
     }
 
-    public void onCompletionOfDownload(String time, String peer_ID) {
-        log(time + ": Peer " + peer_ID + "has downloaded the complete file.");
+    public void onCompletionOfDownload(String time) {
+        log(time + ": Peer " + this.peerID + "has downloaded the complete file.");
     }
 
     public void log(String toPrint) {
-        System.out.println(toPrint);
+        String fileName = "log_peer_" + this.peerID + ".log";
+        try {
+            FileOutputStream outputStream = new FileOutputStream(fileName);
+            byte[] strToBytes = toPrint.getBytes();
+            outputStream.write(strToBytes);
+            outputStream.close();
+        }
+        catch (Exception e) {
+            System.out.println("Oops something bad happened i think");
+        }
     }
 }
--- peerProcess.java
+++ peerProcess.java
@@ -3,9 +3,18 @@
 class peerProcess {
     public static void main(String[] args) {
         if (args.length == 0) {
-            System.out.println("Process number must be provided");
+            System.out.println("Process number must be provided.");
             return;
         }
+        String peerID = args[0];
+        // if peerID is not a positive integer
+        if (!peerID.matches("\\d+")) {
+            System.out.println("Process number must be a positive integer.");
+            return;
+        }
+
         System.out.println("Process " + args[0]);
+
+        Logger logger = new Logger(peerID);
     }
 }
