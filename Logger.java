a6ec20750cf04f745749ec007a7a3ca771fb4ee0
EricNavar <ericnavar@ufl.edu>
2021-09-15 13:38:50 -0400

    reformat more files

--- Logger.java
+++ Logger.java
@@ -1,5 +1,5 @@
 import java.util.ArrayList;
-import java.io.FileOutputStream;  
+import java.io.FileOutputStream;
 
 class Logger {
     String peerID;
@@ -8,15 +8,15 @@ class Logger {
         this.peerID = peerID;
     }
 
-    public void onConnectingTo (String time, String peerID2) {
+    public void onConnectingTo(String time, String peerID2) {
         log(time + ": Peer " + this.peerID + " makes a connection to Peer " + peerID2 + ".");
     }
 
-    public void onConnectingFrom (String time, String peerID2) {
+    public void onConnectingFrom(String time, String peerID2) {
         log(time + ": Peer " + this.peerID + " is connected from Peer " + peerID2 + ".");
     }
 
-    public void onChangeOfPreferredNeighbors (String time, ArrayList<String> preferredNeighbors) {
+    public void onChangeOfPreferredNeighbors(String time, ArrayList<String> preferredNeighbors) {
         StringBuilder toPrint = new StringBuilder(time + ": Peer " + this.peerID + " has the preferred neighbors ");
         for (int i = 0; i < preferredNeighbors.size(); i++) {
             if (i != 0) {
@@ -29,8 +29,9 @@ class Logger {
     }
 
     public void onChangeOfOptimisticallyUnchokedNeighbor(String time, String optimisticallyUnchockedNeighbor_ID) {
-        log(time + ": Peer " + this.peerID + "has the optimistically unchoked neighbor " + optimisticallyUnchockedNeighbor_ID + ".");
-    } 
+        log(time + ": Peer " + this.peerID + "has the optimistically unchoked neighbor "
+                + optimisticallyUnchockedNeighbor_ID + ".");
+    }
 
     public void onUnchoking(String time, String peerID2) {
         log(time + ": Peer " + this.peerID + " is unchoked by " + peerID2 + ".");
@@ -55,8 +56,8 @@ class Logger {
     }
 
     public void onDownloadingAPiece(String time, String peerID2, int piece_index, int number_of_pieces) {
-        log(time + ": Peer " + this.peerID + " has downloaded the piece " + String.valueOf(piece_index) +
-                " from " + peerID2 + ". Now the number of pieces it has is " + String.valueOf(number_of_pieces) + ".");
+        log(time + ": Peer " + this.peerID + " has downloaded the piece " + String.valueOf(piece_index) + " from "
+                + peerID2 + ". Now the number of pieces it has is " + String.valueOf(number_of_pieces) + ".");
     }
 
     public void onCompletionOfDownload(String time) {
@@ -70,8 +71,7 @@ class Logger {
             byte[] strToBytes = toPrint.getBytes();
             outputStream.write(strToBytes);
             outputStream.close();
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("Oops something bad happened i think");
         }
     }
--- peerProcess.java
+++ peerProcess.java
@@ -13,8 +13,9 @@ class peerProcess {
     int pieceCount;
     // denotes which pieces of the file this process has
     static Vector<Boolean> bitfield = new Vector<Boolean>();
-    
-    public peerProcess(String peerId, int numberOfPreferredNeighbors, int unchokingInterval, int optimisticUnchokingInterval, String fileName, int fileSize, int pieceSize) {
+
+    public peerProcess(String peerId, int numberOfPreferredNeighbors, int unchokingInterval,
+            int optimisticUnchokingInterval, String fileName, int fileSize, int pieceSize) {
         logger = new Logger(peerId);
         this.numberOfPreferredNeighbors = numberOfPreferredNeighbors;
         this.unchokingInterval = unchokingInterval;
@@ -28,14 +29,15 @@ class peerProcess {
 
     static Logger logger;
 
-    // this function is to take a string of binary and pad it on the left with zeroes
+    // this function is to take a string of binary and pad it on the left with
+    // zeroes
     // such that it is a certain length.
     static String padWithZeroes(String s, int length) {
         return s + "0".repeat(length - s.length());
     }
 
     static String createHandshakeMessage(int peerId) {
-        //This code is probably not the best
+        // This code is probably not the best
         String result = "P2PFILESHARINGGPROJ";
         char c = 0;
         for (int i = 0; i < 10; i++) {
@@ -47,53 +49,53 @@ class peerProcess {
 
     // no body in message
     static String createChokeMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.CHOKE.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.CHOKE.ordinal()), 8);
         return result;
     }
 
-    //no body in message
+    // no body in message
     static String createUnchokeMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.UNCHOKE.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.UNCHOKE.ordinal()), 8);
         return result;
     }
 
-    //no body in message
+    // no body in message
     static String createInterestedMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.INTERESTED.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.INTERESTED.ordinal()), 8);
         return result;
     }
 
-    //no body in message
+    // no body in message
     static String createNotInterestedMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.NOT_INTERESTED.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.NOT_INTERESTED.ordinal()), 8);
         return result;
     }
 
     static String createHaveMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.HAVE.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.HAVE.ordinal()), 8);
         return result;
     }
 
     static String createBitfieldMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.BITFIELD.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.BITFIELD.ordinal()), 8);
         return result;
     }
 
     static String createRequestMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.REQUEST.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.REQUEST.ordinal()), 8);
         return result;
     }
-    
+
     static String createPieceMessage() {
-        String result = padWithZeroes(Integer.toBinaryString(0),32);
-        result = result + padWithZeroes(Integer.toBinaryString(MessageType.PIECE.ordinal()),8);
+        String result = padWithZeroes(Integer.toBinaryString(0), 32);
+        result = result + padWithZeroes(Integer.toBinaryString(MessageType.PIECE.ordinal()), 8);
         return result;
     }
 
@@ -109,7 +111,7 @@ class peerProcess {
         String fileName = "";
         int fileSize = 0;
         int pieceSize = 0;
-        
+
         try {
             BufferedReader in = new BufferedReader(new FileReader("Common.cfg"));
 
@@ -138,12 +140,12 @@ class peerProcess {
             pieceSize = Integer.parseInt(tokens[1]);
 
             in.close();
-        }
-        catch (Exception ex) {
+        } catch (Exception ex) {
             System.out.println(ex.toString());
         }
 
-        return new peerProcess(peerId, numberOfPreferredNeighbors, unchokingInterval, optimisticUnchokingInterval, fileName, fileSize, pieceSize);
+        return new peerProcess(peerId, numberOfPreferredNeighbors, unchokingInterval, optimisticUnchokingInterval,
+                fileName, fileSize, pieceSize);
     }
 
     private static int GetProcessId(String[] args) {
@@ -157,8 +159,7 @@ class peerProcess {
         try {
             peerId = Integer.parseInt(peerIdString);
             assert peerId >= 0;
-        }
-        catch (Exception e) {
+        } catch (Exception e) {
             System.out.println("The process ID must be a positive integer");
         }
 
@@ -180,21 +181,22 @@ class peerProcess {
             bitfield.set(i, Boolean(srp.hasFile));
         }
 
-        /* TODO:
-        Suppose that peer A tries to make a TCP connection to peer B. Here we describe the
-        behavior of peer A, but peer B should also follow the same procedure as peer A. After
-        the TCP connection is established, peer A sends a handshake message to peer B. It also
-        receives a handshake message from peer B and checks whether peer B is th e right
-        neighbor. The only thing to do is to check whether the handshake header is right and the
-        peer ID is the expected one.
-        After handshaking, peer A sends a ‘bitfield’ message to let peer B know which file pieces
-        it has. Peer B will also send its ‘bitfield’ message to peer A, unless it has no pieces.
-        If peer A receives a ‘bitfield message from peer B and it finds out that peer B has pieces
-        that it doesn doesn’t have, peer A sends ‘interestedinterested’ message to peer B. Otherwise,
-        it sends ‘not interested interested’ message.
-        */
+        /*
+         * TODO: Suppose that peer A tries to make a TCP connection to peer B. Here we
+         * describe the behavior of peer A, but peer B should also follow the same
+         * procedure as peer A. After the TCP connection is established, peer A sends a
+         * handshake message to peer B. It also receives a handshake message from peer B
+         * and checks whether peer B is th e right neighbor. The only thing to do is to
+         * check whether the handshake header is right and the peer ID is the expected
+         * one. After handshaking, peer A sends a ‘bitfield’ message to let peer B know
+         * which file pieces it has. Peer B will also send its ‘bitfield’ message to
+         * peer A, unless it has no pieces. If peer A receives a ‘bitfield message from
+         * peer B and it finds out that peer B has pieces that it doesn doesn’t have,
+         * peer A sends ‘interestedinterested’ message to peer B. Otherwise, it sends
+         * ‘not interested interested’ message.
+         */
 
-        //make list of peerIds that have the file
+        // make list of peerIds that have the file
         if (!srp.hasFile) {
             Vector<Integer> haveFile = new Vector<Integer>();
             for (RemotePeerInfo rpi : srp.peerInfoVector) {
@@ -203,7 +205,7 @@ class peerProcess {
                 }
             }
 
-            //try to handshake with processes that have the file
+            // try to handshake with processes that have the file
             for (Integer i : haveFile) {
                 String messageToSend = createHandshakeMessage(peerId);
             }
