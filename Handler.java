a640492f419e2abc563d723548126e68c18b2ec6
GregBolling <gregmbolling@gmail.com>
2021-12-02 21:55:41 -0500

    structure for all message processing done

--- Handler.java
+++ Handler.java
@@ -108,13 +108,73 @@ public class Handler extends Thread {
 							// wait for incoming Bitfield message 
 							if (WaitForInput(in, 100) == false) {  // see if this was successful
 								CurrentState = 0; // try sending again... it timed out
-							} else { // get incoming message 
+							} else { // get Generic incoming message 
 								dataFromPeer = new byte[in.available()];
 								in.read(dataFromPeer);
 								ByteBuffer buff = ByteBuffer.wrap(dataFromPeer);
-								int bitfieldMsg = Messages.decodeMessage(pp, buff, peerConnected);
-								// received a handshake message from peer
-								this.connectedToPeerIdIncoming = Messages.decodeMessage(pp, buff, -1);
+								int messageDecode = Messages.decodeMessage(pp, buff, peerConnected);
+								int messageLength = GetMessageLength(IncomingMessage);
+								switch(messageDecode) {
+									case MessageType.CHOKE.ordinal():
+										//handleChokeMessage(pp, peerConnected);
+										break;
+									case MessageType.UNCHOKE.ordinal():
+										//handleUnchokeMessage(pp, peerConnected);
+										break;
+									case MessageType.INTERESTED.ordinal():
+										//handleInterestedMessage(pp, peerConnected);
+										break;
+									case MessageType.NOT_INTERESTED.ordinal():
+											//handleNotInterestedMessage(pp, peerConnected);
+										break;
+									case MessageType.HAVE.ordinal():
+											//handleHaveMessage(pp, peerConnected, IncomingMessage);
+										break;
+									case MessageType.BITFIELD.ordinal():
+											//handleBitfieldMessage(IncomingMessage, pp, peerConnected, messageLength);
+										break;
+									case MessageType.REQUEST.ordinal():
+											//handleRequestMessage(pp, peerConnected, IncomingMessage);
+										break;
+									case MessageType.PIECE.ordinal():
+											//handlePieceMessage(pp, peerConnected, messageLength, IncomingMessage);
+										break;
+									default: 
+										// TODO Handle Illegal Messages and send alerts, reset if necessary
+										break;
+								}
+										
+        // The logic for handling the message types are here
+        if (type == MessageType.CHOKE.ordinal()) {
+            // type 0
+            handleChokeMessage(pp, senderPeer);
+        } else if (type == MessageType.UNCHOKE.ordinal()) {
+            // type 1
+            handleUnchokeMessage(pp, senderPeer);
+        } else if (type == MessageType.INTERESTED.ordinal()) {
+            // type 2
+            handleInterestedMessage(pp, senderPeer);
+        } else if (type == MessageType.NOT_INTERESTED.ordinal()) {
+            // type 3
+            handleNotInterestedMessage(pp, senderPeer);
+        } else if (type == MessageType.HAVE.ordinal()) {
+            // type 4
+            handleHaveMessage(pp, senderPeer, IncomingMessage);
+        } else if (type == MessageType.BITFIELD.ordinal()) {
+            // type 5
+            handleBitfieldMessage(IncomingMessage, pp, senderPeer, length);
+        } else if (type == MessageType.REQUEST.ordinal()) {
+            // type 6
+            handleRequestMessage(pp, senderPeer, IncomingMessage);
+        } else if (type == MessageType.PIECE.ordinal()) {
+            // type 7
+            //pp.logger.log("handlePieceMessage(). length of mesage is " + length);
+            handlePieceMessage(pp, senderPeer, length, IncomingMessage);
+        } else {
+            //System.out.println("Invalid message of type " + ParseByte(IncomingMessage, 4));
+            //System.out.println(StandardCharsets.UTF_8.decode(IncomingMessage).toString());
+        }
+
 								this.originalId = this.connectedToPeerIdIncoming;
 								CurrentState++;
 							}
